799d9872503e3d116f77d77ab7c76746839be7c1
Git-X0 <david.barta@student.sps-cl.cz>
2024-12-02 10:06:40 +0100

    modify buttons

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function toggleMenu() {
   const menu = document.querySelector(".quick-menu");
   menu.classList.toggle("opened");
 }
+function RedBLue() {
+  const background = document.querySelector(".top-section");
+  background.style = linear-gradient(to left, #FF0000, #0000FF);
+}
